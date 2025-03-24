b4e5c8c83e0aa0d4ae5d1485d700285f1efb781a
gauravyolsp <gauravyolsp47@gmail.com>
2025-03-24 21:27:54 +0530

    Added Ist code: that represent user defined signal 'SIGUSER1'

--- Gaurav/UserDefinedSignal.c
+++ Gaurav/UserDefinedSignal.c
@@ -5,16 +5,18 @@
 
 void sigCatch(int signo)
 {
-    printf("User defined message %d : signal number : %d\n", getpid(), signo);
+    printf("User defined message PId %d : signal number : %d\n", getpid(), signo);
     signal(SIGUSR1, sigCatch);
 }
 
 int main(void)
 {
     signal(SIGUSR1, sigCatch);
-    if (fork == 0)
+    if (fork() == 0)
     {
         kill(getppid(), SIGUSR1);
     }
+
+    pause();
     return(0);
 }
